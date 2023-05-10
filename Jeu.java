8c34a2c3674db6812694eb414ceb347f63947eec
ZarkyZ <119959785+ZarkyZ@users.noreply.github.com>
2023-05-10 06:49:38 +0200

    Update and rename Jouer to Jeu.java
    schéma de comment procédé

---
+++ Jeu.java
@@ -0,0 +1,17 @@
+//Ce programme permet de jouer au jeu
+public class Jeu {
+
+//on doit pouvoir choisir son mode de jeu
+public void Choix(){
+}
+
+//on code les tours faient par les joueurs pour qu'ils puissent jouer 
+public void Jouer(){
+}
+
+//permettre au meilleur de gagner
+//si l'adversaire ne peut plus placer de piece
+public boolean Gagner(){
+}
+
+}
