b36426fa7e180f7075c332a55f40e5ba1010c888
Beusts <sonic@outlook.fr>
2023-05-12 10:31:50 +0200

    Update Joueur Piece

--- Joueur.java
+++ Joueur.java
@@ -13,5 +13,30 @@ public class Joueur {
     /**
      * Inventaire du joueur
      */
-    Piece [][] piece;
+    Piece [] piece;
+    /**
+     * Crée un joueur avec le nom et le motif donné
+     * @param nom Nom du joueur
+     * @param motif Motif du joueur
+     */
+    Joueur(String nom, String motif, int nbPiece){
+        this.nom = nom;
+        this.motif = motif;
+        piece = new Piece[nbPiece];
+    }
+
+    /**
+     * Crée un joueur par default
+     */
+    Joueur() {
+        this("Joueur", "X ", 10);
+    }
+
+    /**
+     * initialise l'inventaire du joueur
+     */
+    void inventaire(){
+        piece[0] = new Piece("Carre", new boolean[2][2]);
+        piece[0].carre();
+    }
 }
--- Piece.java
+++ Piece.java
@@ -1,3 +1,41 @@
 public class Piece {
+    /**
+     * Nom de la pièce
+     */
+    String nom;
+
+    /**
+     * Motif de la pièce
+     */
+    boolean [][] forme;
+
+    /**
+     * Crée une pièce avec le nom et la forme donnée
+     * @param nom Nom de la pièce
+     * @param forme Forme de la pièce
+     */
+    Piece(String nom, boolean [][] forme){
+        this.nom = nom;
+        this.forme = forme;
+    }
+
+    /**
+     * Crée une pièce par default
+     */
+    Piece() {
+        this("Piece", new boolean[1][1]);
+    }
+
+    /**
+     * Cree une tetraminos carré
+     */
+    void carre(){
+        forme = new boolean[2][2];
+        forme[0][0] = true;
+        forme[0][1] = true;
+        forme[1][0] = true;
+        forme[1][1] = true;
+        this.forme = forme;
+    }
 
 }
