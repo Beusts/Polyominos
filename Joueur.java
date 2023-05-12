d9096c23c84bf65bc7aaff370dc507eb3d17b86a
Beusts <sonic@outlook.fr>
2023-05-12 09:58:47 +0200

    ajout de Ecran + Clavier

---
+++ Joueur.java
@@ -0,0 +1,17 @@
+/**
+ * Classe Joueur
+ */
+public class Joueur {
+    /**
+     * Nom du joueur
+     */
+    String nom;
+    /**
+     * Motif du joueur
+     */
+    String motif;
+    /**
+     * Inventaire du joueur
+     */
+    Piece [][] piece;
+}
